c0f852c4d59310cb9a48ef6b3331d41a064d0339
Connor Kam-Cheong <connorkc42@gmail.com>
2018-11-05 18:18:34 +0000

    Updated Ch6 adventure.rb

--- Ch6/adventure.rb
+++ Ch6/adventure.rb
@@ -2,7 +2,7 @@ location = 0
 
 while location < 2 do
   if location == 0
-    puts "You're facing forward. \n You can go forward, left or right."
+    puts "You wake up in a forest. \n You can go forward, left or right."
   end
 
   direction = gets.chomp
@@ -10,13 +10,13 @@ while location < 2 do
   case direction
   when "right"
     location = 0
-    puts "You encounter a Goblin. You die.\n"
+    puts "You encounter a Goblin. You die."
   when "left"
     location = 0
-    puts "You encounter a Werewolf. You die.\n"
+    puts "You encounter a Werewolf. You die."
   when "forward"
     location += 1
-    puts "You're facing forward."
+    puts "You're facing forward.\n You can go forward, left or right."
   end
 end
 
